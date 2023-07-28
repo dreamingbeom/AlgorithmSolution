cbbfac60033fde2768e391c6feb106213c298351
dreamingbeom <128280944+dreamingbeom@users.noreply.github.com>
2023-07-28 21:37:07 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,25 @@
+
+a = int(input())
+
+for i in range(1, a+1):
+    print(' '*(a-i),end='')
+    print('*'*(2*i-1))
+
+
+for i in range(a-1, 0, -1):
+    print(' '*(a-i),end='')
+    print('*'*(2*i-1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
