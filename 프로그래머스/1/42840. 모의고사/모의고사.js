f40b95120e01358486dfd2a8ba7ec3658e8b5c21
dreamingbeom <128280944+dreamingbeom@users.noreply.github.com>
2024-11-24 19:54:51 +0900

    [level 1] Title: 모의고사, Time: 1.50 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,27 +1,24 @@
 function solution(answers) {
     var answer = [];
-    let num1 = [1, 2, 3, 4, 5]
-    let num2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    let num3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    const num1 = [1, 2, 3, 4, 5]
+    const num2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    const num3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     let cnt = [0, 0, 0]
-    answers.forEach((item, i) => {
-        idx1 = i % num1.length
-        idx2 = i % num2.length
-        idx3 = i % num3.length
-        if (item === num1[idx1]) {
+    answers.forEach((item, idx) => {
+        if (item === num1[idx%5]) {
             cnt[0] += 1
         }
-        if (item === num2[idx2]) {
+        if (item === num2[idx%8]) {
             cnt[1] += 1
         }
-        if (item === num3[idx3]) {
+        if (item === num3[idx%10]) {
             cnt[2] += 1
         }
     })
-    const max = Math.max(...cnt)
-    cnt.forEach((item, i) => {
-        if (max === item) {
-            answer.push(i+1)
+    let max = Math.max(...cnt)
+    cnt.forEach((item, idx) => {
+        if (item === max) {
+            answer.push(idx+1)
         }
     })
     return answer;
